b196df1ed9b31494f859ef09deeebaed9c41a5ae
romes-dev <romes.heriberto@gmail.com>
2024-04-17 20:18:50 -0300

    ok

---
+++ exercicio01.py
@@ -0,0 +1,28 @@
+"""
+            Exercício: Zoológico Interativo
+                        ____
+                   .---'-    \
+      .-----------/           \
+     /           (         ^  |   __
+&   (             \        O  /  / .'
+'._/(              '-'  (.   (_.' /
+     \                    \     ./
+      |    |       |    |/ '._.'
+       )   @).____\|  @ |
+   .  /    /       (    | mrf
+  \|, '_:::\  . ..  '_:::\ ..\).
+
+Objetivo: Desenvolver um programa em Python que simula um zoológico com diferentes tipos de animais, cada um com seus próprios comportamentos para emitir sons, comer e realizar uma atividade característica.
+
+
+
+### Descrição: Você criará uma classe base chamada Animal e várias subclasses representando diferentes tipos de animais. Cada classe terá métodos para emitir_som, comer e atividade. O objetivo é demonstrar polimorfismo ao chamar esses métodos de uma lista de animais e observar comportamentos distintos.
+
+
+### Classe Base Animal:
+Métodos: emitir_som(), comer(), atividade()
+Método __str__ para retornar uma descrição do animal.
+Subclasses (Exemplos: Cachorro, Gato, Pássaro, Elefante):
+Sobrescrever todos os métodos da classe base.
+Adicionar um método atividade que descreve uma ação específica do animal (exemplo: Cachorro pode "buscar bola").
+    """
--- playground.py
+++ playground.py
@@ -24,3 +24,56 @@ zoologico = [Cachorro(), Gato(), Cachorro(), Gato(), Cachorro(), Gato(), Animal(
 for animal in zoologico:
     print(animal)
     print(animal.emitir_som())
+
+
+# Criar um programa em python que simule diferentes animais em um parque com base com comportamento alimentar desses animais e no som que emitem.
+
+class Animal:
+    def __str__(self):
+        return "Animal"
+        
+    def emitir_som(self):
+        return "Este animal emite um som."
+    
+    def comer(self):
+        return "Este animal come."
+    
+class Cachorro(Animal):
+    def __str__(self):
+        return "Cachorro"
+        
+    def emitir_som(self):
+        return "Au au!"
+    
+    def comer(self):
+        return "O cachorro come ração."
+
+class Gato(Animal):
+    def __str__(self):
+        return "Gato"
+        
+    def emitir_som(self):
+        return "Miau!"
+    
+    def comer(self):
+        return "O gato come rato."
+    
+class Passaro(Animal):
+    def __str__(self):
+        return "Pássaro"
+        
+    def emitir_som(self):
+        return "Piu piu!"
+    
+    def comer(self):
+        return "O pássaro come sementes."
+    
+
+parque =[Gato(), Gato(), Passaro(), Cachorro(), Cachorro(), Passaro(), Cachorro(), Gato(), Passaro()]
+
+for animal in parque:
+    print(animal)
+    print(animal.emitir_som())
+    print(animal.comer())
+    print("-"*50)
+    print()
