827e7967397822731e3d482d86b73eb2e9d624d4
romes-dev <romes.heriberto@gmail.com>
2024-04-17 19:28:28 -0300

    first commit

---
+++ # Roteiro de Aula.py
@@ -0,0 +1,35 @@
+# Roteiro de Aula
+
+    """
+1. Introdução à Programação Orientada a Objetos (POO)
+Conceitos Básicos: Explique o que é POO e como ela se diferencia da programação procedural.
+Vantagens da POO: Reutilização de código, modularidade, encapsulamento, etc.
+2. Classes e Objetos
+Definição de Classe: Ensine como definir uma classe em Python usando a palavra-chave class.
+Criando Objetos: Mostre como criar instâncias de uma classe.
+Atributos de Instância: Explique como armazenar dados em objetos.
+Método __init__: Introduza o método construtor para inicializar novos objetos.
+3. Métodos
+Definindo Métodos: Ensine como adicionar comportamentos às classes.
+Métodos de Instância vs. Métodos de Classe vs. Métodos Estáticos: Diferencie esses tipos de métodos e quando usar cada um.
+4. Encapsulamento
+Atributos Privados: Use prefixo de sublinhado duplo (__) para criar atributos privados.
+Getters e Setters: Explique como criar métodos para obter e definir valores, respeitando o encapsulamento.
+5. Herança
+Extendendo Classes: Mostre como uma classe pode herdar de outra, usando exemplos práticos.
+Sobrescrita de Métodos: Explique como e quando sobrescrever métodos da classe pai.
+Função super(): Demonstre como acessar métodos da classe pai que foram sobrescritos.
+6. Polimorfismo
+Conceito de Polimorfismo: Ensine como diferentes classes podem ser usadas de forma intercambiável.
+Métodos Polimórficos: Exemplos práticos de como métodos com o mesmo nome podem realizar ações diferentes.
+7. Composição e Agregação
+Diferenças entre Composição e Agregação: Explique como classes podem ser usadas para construir outras classes.
+8. Práticas Recomendadas
+Nomeação de Classes e Métodos: Dicas sobre como nomear de forma clara e eficaz.
+Documentação: A importância de comentários e docstrings para documentar classes e métodos.
+9. Exercícios Práticos
+Exercícios de Codificação: Proponha exercícios práticos para que os alunos possam implementar o que aprenderam.
+Projetos de Classe: Desafie-os a construir um projeto mais complexo que utilize várias características da POO.
+10. Recursos Adicionais
+Livros e Tutoriais Online: Recomende recursos adicionais para estudo autodirigido.
+    """
